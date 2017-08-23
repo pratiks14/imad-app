357654572bd9e85dfbfa1d8e8471614bbf0190c6
pratiks14 <pratikshetty14@gmail.com>
2017-08-23 18:07:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,13 +9,14 @@ button.onclick=function()
     request.onreadystatechange = function()
     {
         if(request.readystate === XMLHttpRequest.DONE)
+        {
             if(request.status === 200)
             {
                 var counter=request.responseText;
                 var span=document.getElementById("count");
                 span.innerHTML=counter.toString();
             }
-        
+        }
     };
     request.open('GET','http://pratik1rn13cs064.imad.hasura-app.io/counter',true);
     request.send(null);
