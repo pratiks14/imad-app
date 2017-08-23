97bec34f4cf2b02e29da4d4df0841abf8a39c63e
pratiks14 <pratikshetty14@gmail.com>
2017-08-24 02:33:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,14 +8,14 @@ button.onclick=function()
      
     request.onreadystatechange = function()
     {
-        if(request.readystate === XMLHttpRequest.DONE)
+        if(request.readyState === XMLHttpRequest.DONE)
         {
-            //if(request.status === 200)
-            //{
-                var counter=request.responeText;
+            if(request.status === 200)
+            {
+                var counter=request.responseText;
                 var span=document.getElementById("count");
                 span.innerHTML=counter.toString();
-            //} 
+            } 
         }
     };
     
