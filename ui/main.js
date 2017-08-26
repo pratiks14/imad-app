2b397192537e25a00f88fe5bfc8a346afa048a6f
pratiks14 <pratikshetty14@gmail.com>
2017-08-26 14:44:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,21 @@ button.onclick=function()
     request.send(null);
     
 };
+
+var nameInput = document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function()
+{
+    //make request to server and send the name
+    //capture the list of name and render it 
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list+= '<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+};    
+}
