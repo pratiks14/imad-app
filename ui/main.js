effc375621f69962f5751120b4bfac5e64dafeb2
pratiks14 <pratikshetty14@gmail.com>
2017-08-27 01:27:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,7 +39,7 @@ submit.onclick=function()
                 var names=request.responseText;
                 names=JSON.parse(names);
                 var list='';
-                for(var i=0;i<names.length();i++)
+                for(var i=0;i<names.length;i++)
                 {
                     list+= '<li>'+names[i]+'</li>';
                 }
