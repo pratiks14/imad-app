420f032012726d1ce3c5c5c62c9943ce3c4452ad
pratiks14 <pratikshetty14@gmail.com>
2017-08-26 20:34:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,6 +29,26 @@ var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
+    var request=new XMLHttpRequest();//make a request to counter endpoint
+     
+    request.onreadystatechange = function()
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++)
+                {
+                    list+= '<li>'+names[i]+'</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+            } 
+        }
+    };
     //make request to server and send the name
     //capture the list of name and render it 
     var names=['name1','name2','name3'];
@@ -39,4 +59,6 @@ submit.onclick=function()
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
+    request.open('GET','http://pratik1rn13cs064.imad.hasura-app.io/submit-name?'+name,true);
+    request.send(null);
 };    
