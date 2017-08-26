089b707806dfe1e146f5ee8da7b7d959dd4115dd
pratiks14 <pratikshetty14@gmail.com>
2017-08-26 20:55:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,16 +49,6 @@ submit.onclick=function()
             } 
         }
     };
-    //make request to server and send the name
-    //capture the list of name and render it 
-    var names=['name1','name2','name3'];
-    var list='';
-    for(var i=0;i<names.length;i++)
-    {
-        list+= '<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
     request.open('GET','http://pratik1rn13cs064.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
 };    
