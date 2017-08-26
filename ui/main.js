2aca68e0910ccb492b3d85916381be876c136b13
pratiks14 <pratikshetty14@gmail.com>
2017-08-26 20:39:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,13 +39,13 @@ submit.onclick=function()
             {
                 var names=request.responseText;
                 names=JSON.parse(names);
-                var list='';
+                /*var list='';
                 for(var i=0;i<names.length;i++)
                 {
                     list+= '<li>'+names[i]+'</li>';
                 }
                 var ul=document.getElementById('namelist');
-                ul.innerHTML=list;
+                */ul.innerHTML=names;
             } 
         }
     };
