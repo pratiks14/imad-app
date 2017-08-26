969b94266a084ce4c9d17e9cc3222b2ce210663e
pratiks14 <pratikshetty14@gmail.com>
2017-08-27 00:38:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ button.onclick=function()
     
 };
 
-
+var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
     var request=new XMLHttpRequest();//make a request to counter endpoint
@@ -48,7 +48,6 @@ submit.onclick=function()
         }
     };
     var name = document.getElementById('name').value;
-    var submit=document.getElementById('submit_btn');
     request.open('GET','http://pratik1rn13cs064.imad.hasura-app.io/submit-name?name='+myname,true);
     request.send(null);
 };    
