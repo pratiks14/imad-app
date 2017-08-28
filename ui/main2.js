04fa1ca445dae74fd8b3e14b818d47955eaff2db
pratiks14 <pratikshetty14@gmail.com>
2017-08-28 23:29:35 +0530

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -1,1 +1,10 @@
-
+var comments=[];
+var comm_sub=document.getElementById('submit_btn');
+comm_sub.onclick=function()
+{
+    var comment=document.getElementById('comment').innerHTML;
+    comments.push(comment);
+    document.getElementById('comment').innerHTML=comments;
+    
+};
+l
