d5199acf644b8021cc326f5394464ff412e6c5d5
pratiks14 <pratikshetty14@gmail.com>
2017-09-03 22:14:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -41,14 +41,17 @@ app.get('/articles/:articlename',function(req,res)
         {
             res.status(500).send(err.toString());
         }
-        if(result.rows.length === 0)
-        {
-            res.status(404).send("article not found");
-        }
         else
-        {   
-            var articledata=result.rows[0];
-            res.send(articledata);
+        {
+            if(result.rows.length === 0)
+            {
+                res.status(404).send("article not found");
+            }
+            else
+            {   
+                var articledata=result.rows[0];
+                res.send(articledata);
+            }
         }
     });
 
