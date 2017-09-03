4b60b1f116447a13b90fed6a6df9b2e22f9115da
pratiks14 <pratikshetty14@gmail.com>
2017-09-03 19:58:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/test-db',function(req,res)
         }
         else
         {
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     });
     //return a response with the result
