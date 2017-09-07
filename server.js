a57fccc3a3d73c3ed4b23a6c0874cfe75c059c8d
pratiks14 <pratikshetty14@gmail.com>
2017-09-07 14:37:01 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto=require('crypto');
 var Pool=require('pg').Pool;
 var config=
 {
@@ -17,6 +18,17 @@ var pool= new Pool(config);
 var app = express();
 app.use(morgan('combined'));
 
+function hash(input,salt)
+{
+    //crypto.pbkdf2Sync(password, salt, iterations, keylen, digest)
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res)
+{
+    var hashString = hash(req.params.input,'this-is-some-random-string');
+    res.send(hashString);
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
